8dc45e3b07a709d9215db76582853731ffa1ef72
Alex <secret.teamplayer@gmail.com>
2026-01-11 20:00:53 +0200

    Used new hook useGetTodolistsQuery in component

--- src/features/todolists/ui/Todolists/Todolists.tsx
+++ src/features/todolists/ui/Todolists/Todolists.tsx
@@ -2,21 +2,24 @@ import { useAppDispatch, useAppSelector } from "@/common/hooks"
 import { fetchTodolistsTC, selectTodolists } from "@/features/todolists/model/todolists-slice"
 import Grid from "@mui/material/Grid"
 import Paper from "@mui/material/Paper"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { TodolistItem } from "./TodolistItem/TodolistItem"
+import { useGetTodolistsQuery, useLazyGetTodolistsQuery } from "@/features/todolists/api/todolistsApi.ts"
+
+// const todolists = useAppSelector(selectTodolists)
+// const dispatch = useAppDispatch()
+// useEffect(() => {
+//   dispatch(fetchTodolistsTC())
+// }, [])
+
 
 export const Todolists = () => {
-  const todolists = useAppSelector(selectTodolists)
+  const { data } = useGetTodolistsQuery()
 
-  const dispatch = useAppDispatch()
-
-  useEffect(() => {
-    dispatch(fetchTodolistsTC())
-  }, [])
 
   return (
     <>
-      {todolists.map((todolist) => (
+      {data?.map((todolist) => (
         <Grid key={todolist.id}>
           <Paper sx={{ p: "0 20px 20px 20px" }}>
             <TodolistItem todolist={todolist} />
