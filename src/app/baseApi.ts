a9b88d684092812c5df563e6dad7acdd1b148014
Alex <secret.teamplayer@gmail.com>
2026-01-12 15:05:15 +0200

    Created baseApi for all slice-api

---
+++ src/app/baseApi.ts
@@ -0,0 +1,18 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
+import { AUTH_TOKEN } from "@/common/constants"
+
+
+export const baseApi = createApi({
+  reducerPath: 'baseApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: import.meta.env.VITE_BASE_URL,
+    headers: {
+      "API-KEY": import.meta.env.VITE_API_KEY,
+    },
+    prepareHeaders: (headers) => {
+      headers.set("Authorization", `Bearer ${localStorage.getItem(AUTH_TOKEN)}`)
+    },
+  }),
+  tagTypes: ['Todolists'],
+  endpoints: ()=> ({})
+})
