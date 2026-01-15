9eb762421542ebdd2c138a7d19cd395d568b7688
Alex <secret.teamplayer@gmail.com>
2026-01-15 23:51:16 +0200

    A new reducer has been added to check authorization.

--- src/app/app-slice.ts
+++ src/app/app-slice.ts
@@ -7,11 +7,13 @@ export const appSlice = createSlice({
     themeMode: "light" as ThemeMode,
     status: "idle" as RequestStatus,
     error: null as string | null,
+    isLoggedIn: false,
   },
   selectors: {
     selectThemeMode: (state) => state.themeMode,
     selectAppStatus: (state) => state.status,
     selectAppError: (state) => state.error,
+    selectIsLoggedIn: (state) => state.isLoggedIn,
   },
   reducers: (create) => ({
     changeThemeModeAC: create.reducer<{ themeMode: ThemeMode }>((state, action) => {
@@ -23,11 +25,14 @@ export const appSlice = createSlice({
     setAppErrorAC: create.reducer<{ error: string | null }>((state, action) => {
       state.error = action.payload.error
     }),
+    setIsLoggedIn: create.reducer<{isLoggedIn: boolean}>((state, action) => {
+      state.isLoggedIn = action.payload.isLoggedIn
+    }),
   }),
 })
 
-export const { selectThemeMode, selectAppStatus, selectAppError } = appSlice.selectors
-export const { changeThemeModeAC, setAppStatusAC, setAppErrorAC } = appSlice.actions
+export const { selectThemeMode, selectAppStatus, selectAppError, selectIsLoggedIn } = appSlice.selectors
+export const { changeThemeModeAC, setAppStatusAC, setAppErrorAC, setIsLoggedIn } = appSlice.actions
 export const appReducer = appSlice.reducer
 
 export type ThemeMode = "dark" | "light"
