37e8d32926f701f5dc1ff0df06cb8f5967fd5002
Alex <secret.teamplayer@gmail.com>
2026-01-15 23:53:20 +0200

    Written a new syntax RTK Query

--- src/features/auth/api/authApi.ts
+++ src/features/auth/api/authApi.ts
@@ -1,8 +1,31 @@
 import { instance } from "@/common/instance"
 import type { BaseResponse } from "@/common/types"
 import type { LoginInputs } from "@/features/auth/lib/schemas"
+import { baseApi } from "@/app/baseApi.ts"
 
-export const authApi = {
+export const authApi = baseApi.injectEndpoints({
+  endpoints: (build) => {
+    return {
+      me: build.query<BaseResponse<{id: number; email: string; login: string}>, void>({
+        query: () => ({method: 'get', url: "auth/me"}),
+        providesTags: ["Auth"],
+      }),
+      login: build.mutation<BaseResponse<{ userId: number; token: string }>, LoginInputs>({
+        query: (payload) => ({method: 'post', url: "auth/login", body: payload}),
+        invalidatesTags: ["Auth"],
+      }),
+      logout: build.mutation<BaseResponse, void>({
+        query: () => ({method: 'delete', url: "auth/login"}),
+        invalidatesTags: ["Auth"],
+      }),
+  }
+    }
+})
+
+export const { useMeQuery, useLoginMutation, useLogoutMutation } = authApi
+
+
+export const _authApi = {
   login(payload: LoginInputs) {
     return instance.post<BaseResponse<{ userId: number; token: string }>>("auth/login", payload)
   },
